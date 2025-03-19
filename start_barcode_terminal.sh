e72dc5953a8ade1432892040514004980bb11417
fortunatowashu <fortunato@wustl.edu>
2025-03-19 13:52:06 -0500

    Update start_barcode_terminal.sh

--- start_barcode_terminal.sh
+++ start_barcode_terminal.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script opens a terminal window and runs the barcode scanner loop script
-lxterminal -e "/home/washudcm/barcode_project/run_barcode.sh"
+lxterminal -e "/home/pi/barcode_project/run_barcode.sh"
