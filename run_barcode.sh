99031b2f393767f5f844231e7c1c98a135c9b102
fortunatowashu <fortunato@wustl.edu>
2025-03-20 13:21:19 -0500

    Update run_barcode.sh

--- run_barcode.sh
+++ run_barcode.sh
@@ -2,7 +2,7 @@
 # This script continuously runs the barcode_scanner.py script.
 while true; do
     echo "Starting barcode_scanner.py at $(date)"
-    /home/washudcm/barcode_project/venv/bin/python /home/pi/barcode_project/barcode_scanner.py >> /home/pi/barcode_project/barcode_scanner.log 2>&1
+    /home/washudcm/barcode_project/venv/bin/python /home/pi/barcode_project/barcode_scanner.py >> /home/washu/barcode_project/barcode_scanner.log 2>&1
     echo "barcode_scanner.py exited with code $? at $(date). Restarting in 5 seconds..."
     sleep 5
 done
